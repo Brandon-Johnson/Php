1d26b35a2b784f8b6da27806621ef19f5d0023b8
Brandon Johnson <brandon.d.johnson@hotmail.com>
2014-04-23 12:34:13 -0600

    test

--- index.php
+++ index.php
@@ -1,13 +1,67 @@
-<!doctype html>
-<html lang="en">
+<?xml version ="1.0"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <title>Hello World</title>
+  <title>
+    Brandon Johnson's Home Page
+  </title>
+  <object>
+    <h1 style="text-align:center; color:white;12px"> <b>
+        Welcome! <br />
+        To Brandon's Home Page 
+    </b> </h1>
+  </object>
 
-<style>
 
-<body>
+</head>
 
-<p> Hello Everyone!  </p>
+
+<body style="background-color:0F0F10">
+
+
+  <div id="sidebar">
+    <h3 style="color:white;12px">
+      Quote<br />
+    </h3>
+    <object>
+      <blockquote style="color:white;12px; font-family:Comic Sans MS, sans-serif;">
+        <p>
+          Our deepest fear is not that we are inadequate. Our deepest fear is that we are powerful beyond measure. It is our light, not our darkness that most frightens us. We ask ourselves, Who am I to be brilliant, gorgeous, talented, fabulous? Actually, who are you not to be? You are a child of God. Your playing small does not serve the world. There is nothing enlightened about shrinking so that other people won't feel insecure around you. We are all meant to shine, as children do. We were born to make manifest the glory of God that is within us. It's not just in some of us; it's in everyone. And as we let our own light shine, we unconsciously give other people permission to do the same. As we are liberated from our own fear, our presence automatically liberates others.
+        </p>
+      </blockquote>
+    </object>
+    <i style="color:white;12px">
+      by Marianne Williamson
+    </i>
+  </div>
+
+  <div id="main">
+      <h3 style="color:white;12px; font-family:Georgia, serif">
+        About Me:
+      </h3>
+      <dl style="color:white;12px; font-familiy:Georgia, serif; font-size:10pt">
+        <dt> <b>
+            Name:
+        </b> </dt>
+        <dd> <i>
+            Brandon Johnson
+        </i> </dd>   
+        <dt> <b>
+              Major:
+        </b> </dt>
+        <dd> <i>
+            Computer Science
+        </i> </dd>
+        <dt> <b>
+            Hometown:
+        </b> </dt>
+        <dd> <i>
+            Yorba Linda, California
+        </i> </dd>
+      </dl>
+  </div>
+
 </body>
+
+</html>
