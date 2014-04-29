23dc1c502bcf72121901200906298884b14ddbd4
Brandon Johnson <brandon.d.johnson@hotmail.com>
2014-04-29 17:29:43 -0600

    Getting things set up slowly.  Added pictures and basics

--- index.php
+++ index.php
@@ -2,15 +2,32 @@
 
 <html>
   <head>
-    <title>About Me</title>
+    <title>My Home Page</title>
     <link rel="stylesheet" type="text/css" href="http://php-bdj.rhcloud.com/css/homepage.css"/>
   </head>
-  
-  <body>
-  <?php 
-    echo '<p>Hello World</p>'; 
 
-  ?> 
+<body>
+  <div id="pictureOfMe">
+    <img src="http://php-bdj.rhcloud.com/images/me.jpg" alt="I rock these 3d glasses like no other"/>
+  </div> 
+
+  <div id="pictureOfKody">
+    <img src="http://php-bdj.rhcloud.com/images/Kody.jpg" alt="My Best Friend"/>
+  </div>
+
+  <div id="Basics">
+    <dl>
+      <dt><b>Name:</b></dt>
+      <dd><i>Brandon Johnson</dd></i>
+      
+      <dt><b>Major:</b></dt>
+      <dd><i>Computer Science</dd></i>
+      
+      <dt><b>Hometown:</b></dt>
+      <dd><i>Atlanta, Georgia</dd></i>
+
+  </div>
+
 
   </body>
 </html>
