c76d29efaf7baac93c8b0a23390b0f14661b314a
Brandon Johnson <brandon.d.johnson@hotmail.com>
2014-05-12 02:44:40 -0600

    Results

--- Survey.php
+++ Survey.php
@@ -21,22 +21,22 @@
     <div id="Survey">
       <h1>Survey</h1>
       <form action="Results.php" method="post">
-        <label for="os"><h2>Favorite Operating System</h2></lable><br/>
+        <label for="os"><h2>Favorite Operating System</h2></lable>
         <input type="radio" name="os" value="windows">Windows</input><br/>
         <input type="radio" name="os" value="mac">Mac OSx</input><br/>
         <input type="radio" name="os" value="linux">Linux</input><br/>
 
-        <label for="movie"><h2>Favorite Movie Genre</h2></label></br>
+        <label for="movie"><h2>Favorite Movie Genre</h2></label>
         <input type="radio" name="movie" value="comedy">Comedy</input><br/>
         <input type="radio" name="movie" value="action">Action</input><br/>
         <input type="radio" name="movie" value="horror">Horror</input><br/>
 
-        <label for="movie"><h2>Favorite Pizza Topping</h2></label></br>
+        <label for="movie"><h2>Favorite Pizza Topping</h2></label>
         <input type="radio" name="pizza" value="pep">Pepparoni</input><br/>
         <input type="radio" name="pizza" value="bacon">Bacon</input><br/>
         <input type="radio" name="pizza" value="ham">Ham</input><br/>
 
-        <label for="movie"><h2>Major</h2></label></br>
+        <label for="movie"><h2>Major</h2></label>
         <input type="radio" name="major" value="cs">Computer Science</input><br/>
         <input type="radio" name="major" value="cit">CIT</input><br/>
         <input type="radio" name="major" value="we">Web Engineering</input><br/>
