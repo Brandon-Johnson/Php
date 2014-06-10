3bd3323d8fc92ac8aa0606bc283bb5734ddc89cc
Brandon Johnson <brandon.d.johnson@hotmail.com>
2014-06-09 22:35:08 -0600

    Test

--- CustomCar/Home.php
+++ CustomCar/Home.php
@@ -1,62 +1,14 @@
 <!DOCTYPE html>
-
 <html>
   <head>
-    <title>My Home Page</title>
+    <title>Custom Cars</title>
   
-    <link rel="stylesheet" type="text/css" href="/CustomCar/css/SqlHome.css"/>
+    <link rel="stylesheet" type="text/css" href="/CustomCar/css/Store.css"/>
   </head>
 
-<body>
-<?php include '/modules/navigation.php' ?>
-
-
-<?php
-
-echo "<div id=\"main\">";
-try
-{
-  $user = "php";
-  $password = "php";
-  $db = new PDO("mysql:host=127.13.4.130;dbname=php", $user, $password);
-}
-catch (PDOException $ex)
-{
-  echo "Error!: " . $ex->getMessage();
-  die();
-}
-
-foreach ($db->query("SELECT itemName, price, picture, description FROM items") as $row)
-{
-  echo "<div class=\"item\">";
-
-  echo "<div class=\"itemHeader\">";
-  echo $row['itemName'];
-  echo "</div>";
-  
-
-  echo "<div class=\"itemImage\">";
-  echo "<img src=\"" . $row['picture'] . "\"/>";
-  echo "</div>";
-
-  echo "<div class=\"itemPrice\">";
-  echo "<h3> Price: $" . $row['price'] . "</h3>";
-  echo "</div>";
-
-  echo "<div class=\"itemDescription\">";
-  echo "<h3>Description</h3>";
-  echo $row['description'];
-  echo "</div>";
-
-  echo "<br />";
-
-  echo "</div>";
-}
-echo "</div>";
-
- ?>
-
-  
-
+  <body>
+    <?php include '/Modules/Template.php' ?>
+      This is the Homepage
   </body>
+
 </html>
---
+++ CustomCar/Modules/Template.php
@@ -0,0 +1,1 @@
+<link rel="stylesheet" type="text/css" href="/CustomCar/css/template.css"/>
