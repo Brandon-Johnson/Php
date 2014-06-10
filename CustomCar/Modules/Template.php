7355bbfe567cc129d490d1d2856a2a56cd26c2e5
Brandon Johnson <brandon.d.johnson@hotmail.com>
2014-06-10 15:42:31 -0600

    Template

--- CustomCar/Modules/Template.php
+++ CustomCar/Modules/Template.php
@@ -18,10 +18,10 @@
 <div id="navigationBar">
 	<div id="navigationContent">
 		<div class="navigationBox">
-			<a href="https://php-bdj.rhcloud.com/CustomCar/Home.php">Home</a>
+			<a class="navTabs" href="https://php-bdj.rhcloud.com/CustomCar/Home.php">Home</a>
 		</div>
 		<div class="navigationBox">
-			<a href="https://php-bdj.rhcloud.com/CustomCar/Store.php">Store</a>
+			<a class="navTabs" href="https://php-bdj.rhcloud.com/CustomCar/Store.php">Store</a>
 		</div>
 	</div>
 </div>
