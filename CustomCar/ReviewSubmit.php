48f750a0d096eab050757d4700f526bb42325c6f
Brandon Johnson <brandon.d.johnson@hotmail.com>
2014-06-11 02:48:56 -0600

    Template

--- CustomCar/ReviewSubmit.php
+++ CustomCar/ReviewSubmit.php
@@ -5,7 +5,10 @@
 
 <body>
 <?php
-	
+	echo .$_POST["theReview"] . "<br/>";
+    echo .$_COOKIE["itemId"] . "<br/>";
+
+    
 	$user = "php";
 	$password = "php";
 	$db = new mysqli_connect("localhost", $user, $password, "php");
@@ -15,8 +18,7 @@
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
 
-    echo .$_POST["theReview"] . "<br/>";
-    echo .$_COOKIE["itemId"] . "<br/>";
+    
     mysqli_query($db, "INSERT INTO review (review, itemId, userId) VALUES (" . $_POST["theReview"] . ", " . $_COOKIE["itemId"]  . ", 1);");
 
     mysqli_close($db);
