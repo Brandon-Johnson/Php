afcc0a9b9eadbc23135cb27274ab7e65b3376429
Brandon Johnson <brandon.d.johnson@hotmail.com>
2014-06-11 02:56:09 -0600

    Template

--- CustomCar/ReviewSubmit.php
+++ CustomCar/ReviewSubmit.php
@@ -9,20 +9,18 @@
     echo $_COOKIE["itemId"] . "<br/>";
     echo "INSERT INTO review (review, itemId, userId) VALUES (" . $_POST["theReview"] . ", " . $_COOKIE["itemId"]  . ", 1);";
 
-
-	$user = "php";
-	$password = "php";
-	$db = new mysqli_connect("localhost", $user, $password, "php");
-	
- 	if (mysqli_connect_errno())
-    {
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    }
-
-    
-    mysqli_query($db, "INSERT INTO review (review, itemId, userId) VALUES (" . $_POST["theReview"] . ", " . $_COOKIE["itemId"]  . ", 1);");
-
-    mysqli_close($db);
+	try
+	{
+	  $user = "php";
+	  $password = "php";
+	  $db = new PDO("mysql:host=127.13.4.130;dbname=php", $user, $password);
+	}
+	catch (PDOException $ex)
+	{
+	  echo "Error!: " . $ex->getMessage();
+	  die();
+	}
+	$db->query("INSERT INTO review (review, itemId, userId) VALUES (" . $_POST["theReview"] . ", " . $_COOKIE["itemId"]  . ", 1);");
 
     header("location:Item.php?" . $_COOKIE['itemId']);
     die();
