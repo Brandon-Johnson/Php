13b455d8ab9baa40548ead134c1dab468b20b0f7
Brandon Johnson <brandon.d.johnson@hotmail.com>
2014-06-11 02:20:49 -0600

    Template

--- CustomCar/WriteReview.php
+++ CustomCar/WriteReview.php
@@ -9,18 +9,16 @@
 
  <div class="mainBodyContent">
    <form method="POST" action="ReviewSubmit.php">
-   		Write Review<br/>
+   		Write Review For 
+   		<?php
+   		foreach ($db->query("SELECT itemName FROM items WHERE id=" . $_GET['itemId']) as $row)
+   			echo $row['itemName'];
+   		?>
+   		<br/>
    		<textarea rows="20" cols="100" name="theReview">Write Review Here</textarea>
    		</br>
    		<input type="submit" value="submitReview"/>
    	</form>
-
-     
-
-
-     <?php
-     	echo $_COOKIE["itemId"];
-     ?>
  </div>
      <?php include 'Modules/Template.php' ?>
   </body>
