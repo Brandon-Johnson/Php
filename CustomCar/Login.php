878cc63fc63195a5d0f9d62fbfc570a303c18431
Brandon Johnson <brandon.d.johnson@hotmail.com>
2014-06-11 03:41:50 -0600

    Template

--- CustomCar/Login.php
+++ CustomCar/Login.php
@@ -10,9 +10,9 @@
     <div class="mainBodyContent">
        <form action="validate.php" method="POST">
        	<label for="userName">UserName: </label>
-       	<input type="text" name="userName" id="userName"/>
+       	<input type="text" name="userName" id="userName"/><br/>
        	<label for="password">Password: </label>
-       	<input type="password" name="password" id="password"/>
+       	<input type="password" name="password" id="password"/><br/>
        	<input type="submit" value="Login"/>
        </form>
     </div>  
--- CustomCar/validate.php
+++ CustomCar/validate.php
@@ -1,4 +1,20 @@
 <?php
 echo $_POST['userName'];
 echo $_POST['password'];
+$userName = $_POST['userName'];
+$pass = $_POST['password'];
+try
+{
+  $user = "php";
+  $password = "php";
+  $db = new PDO("mysql:host=127.13.4.130;dbname=php", $user, $password);
+}
+catch (PDOException $ex)
+{
+  echo "Error!: " . $ex->getMessage();
+  die();
+}
+
+$userId = $db->prepare("SELECT id FROM users WHERE username = '$userName' AND password = 'pass'");
+echo $userId;
 ?>
