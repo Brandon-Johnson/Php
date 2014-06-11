4d1b8f28ccbe40035af9fb4ab65f5d0522f2fdcb
Brandon Johnson <brandon.d.johnson@hotmail.com>
2014-06-11 04:15:33 -0600

    Template

--- CustomCar/validate.php
+++ CustomCar/validate.php
@@ -1,6 +1,4 @@
 <?php
-echo $_POST['userName'];
-echo $_POST['password'];
 $userName = $_POST['userName'];
 $pass = $_POST['password'];
 
@@ -15,10 +13,6 @@ catch (PDOException $ex)
   echo "Error!: " . $ex->getMessage();
   die();
 }
-
-echo $userName;
-echo $pass;
-echo "<br/>";
 
 foreach ($db->query("SELECT id FROM user WHERE username = '$userName' AND password = '$pass'") as $row)
 	$row['id'];
